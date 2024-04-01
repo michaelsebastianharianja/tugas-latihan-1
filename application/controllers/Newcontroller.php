809ffb0ec67c39b2d37d1c60c77a40b11dbdfc5c
michaelsebastianharianja <michaelsebastianhat@gmail.com>
2024-04-01 20:41:39 +0700

    upload tugas latihan pertemuan 3

---
+++ application/controllers/Newcontroller.php
@@ -0,0 +1,9 @@
+<?php
+class Newcontroller extends CI_controller
+{
+    public function Index (){
+        echo "<h1>Perkenalkan</h1>";
+        echo "nama saya michael saya tinggal di percetakan";
+    }
+}
+
